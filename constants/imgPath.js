d5e63c623e9e9982e5f6890a31a279448c9ab16f
nelobrero <137615841+nelobrero@users.noreply.github.com>
2024-04-19 11:20:42 +0800

    Add files via upload

---
+++ constants/imgPath.js
@@ -0,0 +1,6 @@
+export default {
+    icCurLoc: require('../assets/Oval.png'),
+    icGreenMarker: require("../assets/pin (1).png"),
+    greenIndicator: require("../assets/icon13.png"),
+    icBike: require("../assets/pin2.png"),
+}
